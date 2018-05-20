853d89c26645753f78739c903f2655613b83692c
Daniel Stewart <mgdog1@googlemail.com>
2018-05-20 23:11:48 +0100

    working on vacant room method in hotel

--- src/main/java/hotel/Hotel.java
+++ src/main/java/hotel/Hotel.java
@@ -1,4 +1,42 @@
 package hotel;
 
+import hotel.rooms.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hotel {
+
+    private List<Room> rooms;
+    private double funds;
+
+    public Hotel(List<Room> rooms, double funds){
+        if(rooms.size() < 1) throw new IllegalArgumentException("Hotel must contain rooms");
+        if(funds < 0) throw new IllegalArgumentException("Hotel must have funds");
+        this.rooms = new ArrayList<>(rooms);
+        this.funds = funds;
+    }
+
+    public int roomCount(){
+        return rooms.size();
+    }
+
+    public double getFunds(){
+        return this.funds;
+    }
+
+    public void bookRoom(Booking booking, Room room){
+        if(rooms.contains(room)){
+            room.setBooking(booking);
+        }
+    }
+
+    public Room[] getVacantRooms(){
+        return (Room[]) rooms.stream().filter(room -> !room.hasBooking()).toArray();
+    }
+
+    public Room[] getActiveBookings(){
+        return  rooms.stream().filter(room -> !room.hasBooking()).toArray(Room[]);
+    }
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,75 @@
+import hotel.Booking;
+import hotel.Guest;
+import hotel.Hotel;
+import hotel.enums.BedroomType;
+import hotel.rooms.Bedroom;
+import hotel.rooms.ConferenceRoom;
+import hotel.rooms.Room;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+
+    ArrayList<Room> rooms;
+    Hotel hotel;
+
+    @Before
+    public void setup(){
+        Room room1 = new Bedroom(2, BedroomType.DOUBLE);
+        Room room2 = new ConferenceRoom("Washington Room",10,1);
+        rooms = new ArrayList<>();
+        rooms.add(room1);
+        rooms.add(room2);
+        hotel = new Hotel(rooms,10);
+
+        Guest guest1 = new Guest("Matthew", 200);
+        Guest guest2 = new Guest("Amy", 100);
+        ArrayList<Guest> guests = new ArrayList<>();
+        guests.add(guest1);
+        guests.add(guest2);
+        Booking booking = new Booking(2, guests);
+        hotel.bookRoom(booking, room1);
+    }
+
+    @Test
+    public void hasRooms(){
+        assertEquals(2, hotel.roomCount());
+    }
+
+    @Test
+    public void checkRoomsCopied(){
+        Room room = new ConferenceRoom("Lincoln Room",100,12);
+        rooms.add(room);
+        assertEquals(2, hotel.roomCount());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void cannotCreateHotelWithNoRooms(){
+        hotel = new Hotel(new ArrayList<Room>(), 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void cannotCreateHotelWithNegativeFunds(){
+        hotel = new Hotel(rooms, -1);
+    }
+
+    @Test
+    public void hasFunds(){
+        assertEquals(10, hotel.getFunds(), 0.01);
+    }
+
+    @Test
+    public void hasVacantRooms(){
+        assertEquals(1, hotel.getVacantRooms().length);
+    }
+
+    @Test
+    public void hasActiveBookings(){
+        assertEquals(1, hotel.getActiveBookings().length);
+    }
+
 }
